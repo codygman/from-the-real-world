a2df337271203f743dba3f11246e2ecdcd703e38
Cody Goodman <codygman.consulting@gmail.com>
2014-01-29 01:12:52 -0600

    Write simple logic for parsing current csv input data

--- mistrut.hs
+++ mistrut.hs
@@ -1,2 +1,24 @@
+import Text.CSV
+import Data.String.Utils
+import System.FilePath
+
+-- compose all csv pre-processing functions together
+clean :: String -> String
+clean = fixQuoting
+
+-- Our malformed csv file uses '\"' to escape " within a text quoted column
+-- it should use "" as per rfc: rwww.rfc-editor.org/rfc/rfc4180.txt
+fixQuoting :: String -> String
+fixQuoting = replace "\\\"" "\"\""
+
 main :: IO ()
-main = putStrLn "Will be disproven"
+main = do
+  input <- readFile "contractor-customer-scrape-data.csv"
+  let cleaned_input = clean input
+  let error_fp = "." </> "errors.txt"
+  let parsed_data = parseCSV error_fp cleaned_input
+  case parsed_data of
+    Left _ -> error "something went wrong"
+    Right results -> do
+                     putStrLn $ printCSV results
+

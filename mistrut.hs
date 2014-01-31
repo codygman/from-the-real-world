72c8f530ee49588bc968aa1f092f5b4b23e859c0
Cody Goodman <codygman.consulting@gmail.com>
2014-01-30 19:25:00 -0600

    Use lazy bytestrings, text, and parsing
    This takes out functionality of "fixQuoting" for now. It will be added
    after I add logic to convert the lazy bytestrings to lazy text.

--- mistrut.hs
+++ mistrut.hs
@@ -1,8 +1,11 @@
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as E
-import qualified Data.ByteString as BS
+{-# LANGUAGE OverloadedStrings #-}
+import Data.Csv
 import System.FilePath
-import Text.CSV
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Encoding as E
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString.Lazy.Char8 as BLC
+import qualified Data.Vector as V
 
 -- compose all csv pre-processing functions together
 clean :: T.Text -> T.Text
@@ -11,17 +14,13 @@ clean = fixQuoting
 -- Our malformed csv file uses '\"' to escape " within a text quoted column
 -- it should use "" as per rfc: rwww.rfc-editor.org/rfc/rfc4180.txt
 fixQuoting :: T.Text -> T.Text
-fixQuoting = T.replace (T.pack "\\\"") (T.pack "\"\"")
+fixQuoting = T.replace ("\\\"") ("\"\"")
 
 main :: IO ()
 main = do
-  input <- BS.readFile "impossible-to-parse.csv" :: IO BS.ByteString
-  let cleaned_input = clean $ E.decodeUtf8 input
-  let error_fp = "" </> "errors.txt"
-  let parsed_data = parseCSV error_fp $ T.unpack cleaned_input
-  case parsed_data of
-    Left _ -> error "something went wrong"
-    Right results -> do
-                     let fields = head results :: [Field]
-                     putStrLn $ show $ length fields
-
+  let filename = "test1.csv"
+  input <- BS.readFile filename
+  let parsedCsv = decode NoHeader input :: Either String (V.Vector (V.Vector BLC.ByteString))
+  case parsedCsv of
+    Left err -> putStrLn err
+    Right v ->  putStrLn $ "There are " ++ show (V.length v) ++ " columns in " ++ show filename
